bf2ac55a8e32b87e69ef6b8610f50270b6a4eb97
Reetik Patel <Reetik2000@yahoo.com>
2022-10-08 23:32:31 -0500

    ♻️ Abstracted pager fn

---
+++ pager.js
@@ -0,0 +1,33 @@
+const request = require('request');
+const secrets = require('./secrets');
+
+const sendPage = async () => {
+    const pagerUrl = "https://enceledus.xmatters.com/api/integration/1/functions/f2a069d9-46f6-40c2-bfd9-fd4a3a852d15/triggers";
+
+    const headers = {
+        "Content-Type": "application/json"
+    }
+
+    const body = JSON.stringify({
+        priority: "MEDIUM"
+    });
+    console.log(secrets);
+    const options = {
+        method: "POST",
+        url: pagerUrl,
+        headers,
+        body,
+        auth: {
+            user: secrets.xm.user,
+            password: secrets.xm.pass
+        }
+    }
+
+    request(options, (err, res) => {
+        if(err) console.error(err);
+        console.log(res.body);
+        return;
+    })
+}
+
+module.exports = { sendPage };
