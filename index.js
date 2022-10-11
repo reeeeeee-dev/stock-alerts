83e58b1400762ce53bed1b5d1bce9b82cd9026c6
Reetik Patel <Reetik2000@yahoo.com>
2022-10-11 01:12:00 -0500

    Revert "⚡ Limited to 30 min at 6am"
    This reverts commit 3b548803e650586d550d33f89789701c2d404c7e.

--- index.js
+++ index.js
@@ -135,18 +135,16 @@ const run = async () => {
     await page.goto("https://store.ui.com/collections/unifi-protect/products/g4-doorbell-pro");
     const loop = async () => {
         const time = new Date();
-        if(time.getHours() === 6 && time.getMinutes() <= 30) {
-            checkStock(page).then(async stock => {
-                console.log(`Stock check Time: ${((new Date()) - time) / 1000}s`);
-                if(stock) {
-                    await login(page);
-                    pager.sendPage();
-                    buyItem(page);
-                } else {
-                    setTimeout(loop, 2000);
-                }
-            });
-        }
+        checkStock(page).then(async stock => {
+            console.log(`Stock check Time: ${((new Date()) - time) / 1000}s`);
+            if(stock) {
+                await login(page);
+                pager.sendPage();
+                buyItem(page);
+            } else {
+                setTimeout(loop, 500)
+            }
+        });
     }
 
     loop();
